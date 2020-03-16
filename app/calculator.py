5f91167e6c7f2ea2bdbd38b519cc9440702fb556
Casey <joh14140@umn.edu>
2020-03-16 12:39:13 -0500

    adding button connection

--- app/calculator.py
+++ app/calculator.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def test():
-	# return "Hello"
+def initial_page():
+	return render_template("calculator_page.html")
+
+@app.route("/", methods=['POST'])
+def calculation():
+	eq = request.form['equation']
+	if eq:
+		return json.dumps({'html':'<span>It worked</span>'})
 	return render_template("calculator_page.html")
 
