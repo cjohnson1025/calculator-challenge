169cd5b24e5783701f80c1692c085ef562327748
Casey <joh14140@umn.edu>
2020-03-16 10:29:26 -0500

    adding html

--- app/calculator.py
+++ app/calculator.py
@@ -1,7 +1,7 @@
-from flask import Flask 
+from flask import Flask, render_template 
 app = Flask(__name__)
 
 @app.route("/")
 def test():
-	return "Hello world!"
+	return render_template("calculator_page.html")
 
