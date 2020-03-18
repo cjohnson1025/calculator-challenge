8c75833766aeeedea7b3b5b124c84390fa266b85
Casey <caseyajohnson1025@gmail.com>
2020-03-18 09:07:11 -0500

    added computations variable to render_template

--- app/calculator.py
+++ app/calculator.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+computations = [4+5=9];
+
 @app.route("/")
 def initial_page():
-	return render_template("calculator_page.html")
+	return render_template("calculator_page.html", computations=computations)
 
 @app.route("/", methods=['POST'])
 def calculation():
 	eq = request.form['equation']
 	if eq:
-		return json.dumps({'html':'<span>It worked</span>'})
-	return render_template("calculator_page.html")
+		pass
+	return render_template("calculator_page.html", computations=computations)
 
 def compute(input):
 
