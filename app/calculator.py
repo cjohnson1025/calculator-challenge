79ebd934ffa09e7b21d65f35e97cfba537760291
Casey <caseyajohnson1025@gmail.com>
2020-03-18 11:51:13 -0500

    debugging

--- app/calculator.py
+++ app/calculator.py
@@ -11,7 +11,7 @@ def initial_page():
 @app.route("/", methods=['POST'])
 def calculation():
 	eq = request.form['equation']
-	compute(str(eq))
+	# compute(str(eq))
 	return render_template("calculator_page.html", computations=computations)
 
 def compute(input):
