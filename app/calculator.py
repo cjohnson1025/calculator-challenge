2ca0429f4298531b65889798165adbad44c6345a
Casey <caseyajohnson1025@gmail.com>
2020-03-18 12:17:28 -0500

    debugging

--- app/calculator.py
+++ app/calculator.py
@@ -18,60 +18,61 @@ def calculation():
 def compute(input):
 
     #list of chars that are accepted by the calculator
-    validChars = "0123456789/*-+."
+    # validChars = "0123456789/*-+."
 
-    #split the input string into a list of characters
-    inputList = [char for char in input];
+    # #split the input string into a list of characters
+    # inputList = [char for char in input];
 
-    #checks to see if input is valid
-    for char in inputList:
-        if char not in validChars:
-            return "Not a valid input"
+    # #checks to see if input is valid
+    # for char in inputList:
+    #     if char not in validChars:
+    #         return "Not a valid input"
 
 
-    computeStack = [];
-    while inputList != []:
-        nextChar = inputList[-1]
+    # computeStack = [];
+    # while inputList != []:
+    #     nextChar = inputList[-1]
 
-        #if next character is a digit must check previous indices to find when the number ends
-        if inputList[-1].isdigit(): 
-            i = 0
-            while i < len(inputList) and (inputList[-1-i].isdigit() or inputList[-1-i] == '.'):
-                i += 1;
-            nextChar = inputList[-i:]
-        computeStack.append(nextChar)
+    #     #if next character is a digit must check previous indices to find when the number ends
+    #     if inputList[-1].isdigit(): 
+    #         i = 0
+    #         while i < len(inputList) and (inputList[-1-i].isdigit() or inputList[-1-i] == '.'):
+    #             i += 1;
+    #         nextChar = inputList[-i:]
+    #     computeStack.append(nextChar)
 
-        if len(computeStack) > 1:
-            if computeStack[-2] == '*':
-                lhs = "".join(computeStack.pop());
-                computeStack.pop();
-                rhs = "".join(computeStack.pop());
-                computeStack.append([str(double(lhs)*double(rhs))])
-            elif computeStack[-2] == '/':
-                lhs = "".join(computeStack.pop());
-                computeStack.pop();
-                rhs = "".join(computeStack.pop());
-                computeStack.append([str(double(lhs)/double(rhs))])
+    #     if len(computeStack) > 1:
+    #         if computeStack[-2] == '*':
+    #             lhs = "".join(computeStack.pop());
+    #             computeStack.pop();
+    #             rhs = "".join(computeStack.pop());
+    #             computeStack.append([str(double(lhs)*double(rhs))])
+    #         elif computeStack[-2] == '/':
+    #             lhs = "".join(computeStack.pop());
+    #             computeStack.pop();
+    #             rhs = "".join(computeStack.pop());
+    #             computeStack.append([str(double(lhs)/double(rhs))])
                 
-        inputList = inputList[:-i]
+    #     inputList = inputList[:-i]
        
 
-    while len(computeStack) > 1:
-        if computeStack[-2] == '+':
-            lhs = "".join(computeStack.pop());
-            computeStack.pop();
-            rhs = "".join(computeStack.pop());
-            computeStack.append([str(double(lhs)+double(rhs))])
-        elif computeStack[-2] == '-':
-            lhs = "".join(computeStack.pop());
-            computeStack.pop();
-            rhs = "".join(computeStack.pop());
-            computeStack.append([str(double(lhs)-double(rhs))])
-    if len(computations) > 9:
-        computations.pop();
-    computations.insert(0, input + " = " + str(computeStack[0][0]))
+    # while len(computeStack) > 1:
+    #     if computeStack[-2] == '+':
+    #         lhs = "".join(computeStack.pop());
+    #         computeStack.pop();
+    #         rhs = "".join(computeStack.pop());
+    #         computeStack.append([str(double(lhs)+double(rhs))])
+    #     elif computeStack[-2] == '-':
+    #         lhs = "".join(computeStack.pop());
+    #         computeStack.pop();
+    #         rhs = "".join(computeStack.pop());
+    #         computeStack.append([str(double(lhs)-double(rhs))])
+    # if len(computations) > 9:
+    #     computations.pop();
+    # computations.insert(0, input + " = " + str(computeStack[0][0]))
 
-    return computeStack[0][0]
+    # return computeStack[0][0]
+    pass
 
 #TODO:
 #connect number/computation buttons to input box
